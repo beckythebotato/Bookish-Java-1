f82ee35aca541029a3ef5f79c4088705b9f79d4e
Gregory Barratt <barrattg@hotmail.co.uk>
2021-10-08 14:20:39 +0100

    working getusers script in main

--- src/main/java/org/softwire/training/bookish/Main.java
+++ src/main/java/org/softwire/training/bookish/Main.java
@@ -1,10 +1,13 @@
 package org.softwire.training.bookish;
 
 import org.jdbi.v3.core.Jdbi;
+import org.softwire.training.bookish.models.database.User;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.SQLOutput;
+import java.util.List;
 
 
 public class Main {
@@ -16,11 +19,14 @@ public class Main {
         String password = "bookish";
         String connectionString = "jdbc:mysql://" + hostname + "/" + database + "?user=" + user + "&password=" + password + "&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=GMT&useSSL=false";
 
-        jdbcMethod(connectionString);
-        jdbiMethod(connectionString);
+        //jdbcMethod(connectionString);
+        getUsers(jdbiMethod(connectionString)).forEach(u->{System.out.println(u);} );
+        //getUsers(jdbiMethod(connectionString));
+
+
     }
 
-    private static void jdbcMethod(String connectionString) throws SQLException {
+    /*private static void jdbcMethod(String connectionString) throws SQLException {
         System.out.println("JDBC method...");
 
         // TODO: print out the details of all the books (using JDBC)
@@ -29,19 +35,43 @@ public class Main {
         Connection connection = DriverManager.getConnection(connectionString);
 
 
+    }*/
 
-    }
+    private static Jdbi jdbiMethod(String connectionString) throws SQLException {
+        System.out.println("JDBI method...");
 
-    private static void jdbiMethod(String connectionString) {
-        System.out.println("\nJDBI method...");
 
-        // TODO: print out the details of all the books (using JDBI)
-        // See this page for details: http://jdbi.org
-        // Use the "Book" class that we've created for you (in the models.database folder)
+        Connection connection = DriverManager.getConnection(connectionString);
 
         Jdbi jdbi = Jdbi.create(connectionString);
 
+        return jdbi;
+    }
+
+
+
+     static List<User> getUsers(Jdbi jdbi) {
+
+
+
+        return jdbi.withHandle(handle ->
+
+                handle.createQuery("Select * FROM users")
+                        .mapToBean(User.class)
+                        .list()
+
+
+        );
+
 
 
     }
+
+
+
+
 }
+
+
+
+
